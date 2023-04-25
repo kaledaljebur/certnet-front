abbb6c947f0a9e73cf804bf0d80f11d371eb205b
Kaled Aljebur <kaledaljebur@gmail.com>
2023-04-25 16:50:53 +1000

    Project Start Commit

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,18 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Fact Certification Network</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Fact Certification Network" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p>If you see this page, the project is still under construction.</p>
+        <p>Thank you for visiting.</p>
+        <p><i>Kaled Aljebur</i></p>
+
+          {/* <code>pages/index.js</code> */}
         </p>
       </main>
 
